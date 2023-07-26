b72114ef716755c7691acc75d6adcc476262791b
“[Nazmul <[nazmul.ahammed.nz@gmail.com]>
2023-07-26 21:24:12 +0600

    Frontend & Backend

---
+++ Front-end (Next js)/pages/_app.js
@@ -0,0 +1,9 @@
+import '@/styles/globals.css'
+import Navbar from './component/navbar'
+
+export default function App({ Component, pageProps }) {
+  return(<>
+  <Navbar/>
+  <Component {...pageProps} />
+  </>)
+}
